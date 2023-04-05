dcbec369f1bf071eee44cb7c56176f89ea55befa
FredericoFilho <fgogf@ic.ufal.br>
2023-04-05 02:07:39 -0300

    Add files via upload

--- src/main/java/grupo/quatro/api_manage_escola/controller/ProfessorController.java
+++ src/main/java/grupo/quatro/api_manage_escola/controller/ProfessorController.java
@@ -2,10 +2,8 @@ package grupo.quatro.api_manage_escola.controller;
 
 import grupo.quatro.api_manage_escola.Aluno.Aluno;
 import grupo.quatro.api_manage_escola.Aluno.DadosCadastroAluno;
-import grupo.quatro.api_manage_escola.Professor.DadosAtualizacaoProfessor;
-import grupo.quatro.api_manage_escola.Professor.DadosCadastroProfessor;
-import grupo.quatro.api_manage_escola.Professor.Professor;
-import grupo.quatro.api_manage_escola.Professor.ProfessorRepository;
+import grupo.quatro.api_manage_escola.Aluno.DadosListagemAluno;
+import grupo.quatro.api_manage_escola.Professor.*;
 import grupo.quatro.api_manage_escola.Turma.Turma;
 import grupo.quatro.api_manage_escola.UsuarioCredentials.UsuarioCredentials;
 import grupo.quatro.api_manage_escola.UsuarioCredentials.UsuarioCredentialsRepository;
@@ -15,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
 
@@ -23,21 +22,26 @@ import java.util.SortedSet;
 public class ProfessorController {
 
     @Autowired
-    private ProfessorRepository repository;
+    private ProfessorRepository professorRepository;
     @Autowired
     private UsuarioCredentialsRepository usuarioCredentialsRepository;
 
     @PostMapping
     @Transactional
     public void cadastro(@RequestBody @Valid DadosCadastroProfessor dados) {
-        repository.save(new Professor(dados));
+        Professor professor = professorRepository.save(new Professor(dados));
         usuarioCredentialsRepository.save(new UsuarioCredentials(dados.cpf(), dados.senha(), dados.userType()));
+    }
+
+    @GetMapping
+    public List<DadosListagemProfessor> listar() {
+        return professorRepository.findAll().stream().map(DadosListagemProfessor::new).toList();
     }
 
     @PutMapping
     @Transactional
     public void atualizar(@RequestBody @Valid DadosAtualizacaoProfessor dados) {
-        Professor professor = repository.getReferenceById(dados.id());
+        Professor professor = professorRepository.getReferenceById(dados.id());
         professor.updateInfo(dados);
     }
 
--- src/main/java/grupo/quatro/api_manage_escola/controller/TurmaController.java
+++ src/main/java/grupo/quatro/api_manage_escola/controller/TurmaController.java
@@ -2,17 +2,18 @@ package grupo.quatro.api_manage_escola.controller;
 
 import grupo.quatro.api_manage_escola.Aluno.Aluno;
 import grupo.quatro.api_manage_escola.Aluno.DadosCadastroAluno;
+import grupo.quatro.api_manage_escola.Aluno.DadosListagemAluno;
 import grupo.quatro.api_manage_escola.Turma.DadosCadastroTurma;
+import grupo.quatro.api_manage_escola.Turma.DadosListagemTurma;
 import grupo.quatro.api_manage_escola.Turma.Turma;
 import grupo.quatro.api_manage_escola.Turma.TurmaRepository;
 import grupo.quatro.api_manage_escola.UsuarioCredentials.UsuarioCredentials;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/turma")
@@ -25,6 +26,11 @@ public class TurmaController {
     @Transactional
     void cadastrar(@RequestBody @Valid DadosCadastroTurma dados) {
         Turma turma = turmaRepository.save(new Turma(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemTurma> listar() {
+        return turmaRepository.findAll().stream().map(DadosListagemTurma::new).toList();
     }
 
 //    @PostMapping("/adicionar_professor")
