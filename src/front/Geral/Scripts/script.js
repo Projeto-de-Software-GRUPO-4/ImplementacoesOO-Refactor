01428eb11baa0956bfe639c9942632073d8d535e
Renato Alves <108096649+rena02to@users.noreply.github.com>
2023-04-07 20:44:55 -0300

    Add files via upload

--- src/front/Geral/Scripts/script.js
+++ src/front/Geral/Scripts/script.js
@@ -24,3 +24,17 @@ function mostrarRepita(){
         senhaInput.type = "password";
     }
 }
+
+function errado(){
+    const error = document.getElementById('error');
+    var cpf = document.getElementById('cpf').value;
+    var senha = document.getElementById('senha').value;
+
+    if(cpf === ""){
+        error.textContent = "O campo de CPF é obrigatório!";
+    }else if(senha === ""){
+        error.textContent = "O campo de senha é obrigatório!";
+    }else{
+        error.textContent = "Senha incorreta! (falta verificar no DB)";
+    }
+}
